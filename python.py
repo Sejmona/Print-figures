04817a84889c94292bbb9a72073489e3498f9f1d
Simona Šmelhausová <101262454+Sejmona@users.noreply.github.com>
2024-07-05 19:10:19 +0200

    Update python.py
    Second level - here is the second square from the task.

--- python.py
+++ python.py
@@ -8,7 +8,14 @@ def print_shape(choice, size):
                 else:
                     print(' ', end='')
             print()
- 
+    elif choice == 'b':
+        for i in range(size):
+            for j in range(size):
+                if i <= j:
+                    print('*', end='')
+                else:
+                    print(' ', end='')
+            print() 
     else:
         print("Invalid choice")
 
